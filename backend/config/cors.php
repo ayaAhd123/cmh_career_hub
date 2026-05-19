53a729cb82b8525627dea7e85cce8143cba1bef2
Itsayael9 <elouahabiaya1@gmail.com>
2026-05-19 12:12:59 +0000

    Save backend and API changes to backend branch

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,6 +6,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'https://localhost:5173',
+        'http://localhost:8081',
+        'https://localhost:8081',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
