6ac63be322136a8cdb04b1bdf479e955e07d4632
ayaAhd123 <aya.soltana.90@gmail.com>
2026-05-22 10:03:44 +0000

    fix: change localhost link

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,19 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'https://localhost:5173',
-        'http://localhost:8081',
-        'https://localhost:8081',
-        'http://localhost:8080',
-        'http://127.0.0.1:8080',
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
-    ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => ['#^https://.*\.app\.github\.dev$#'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
